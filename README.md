2fd23b3cec17edb757ee1cc8b59f2e4bef567b90
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-09-30 12:18:44 -0700

    feat(core): code cleanup, readme changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AspNetCoreEntityFrameworkOData
+
 .Net 5.x implementation of Asp.Net Web Api with OData and Entity Framework support
--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,7 @@ namespace AspNetCoreEntityFrameworkOData
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MyDbContext>(opt => opt.UseInMemoryDatabase($"InMemory"), ServiceLifetime.Singleton);
+            services.AddDbContext<MyDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
 
             services.AddControllers();
 
